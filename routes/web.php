9ecefcc829a6cd38de9da9eea1723ca15ad721d6
Your Name <you@example.com>
2022-02-02 09:52:48 -0600

    PROYECTO

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('inicio');
 });
+
+Route::get('/tema1', function(){
+    return view('generic');
+});
+
+Route::get('/tema2', function(){
+    return view('generic1');
+});
+
+Route::get('/tema3', function(){
+    return view('generic2');
+});
+
+Route::get('/tema4', function(){
+    return view('generic3');
+});
+
+Route::get('/tema5', function(){
+    return view('generic4');
+});
+
+Route::get('/tema6', function(){
+    return view('generic5');
+});
+
+Route::get('/tema7', function(){
+    return view('generic6');
+});
+
+Route::get('/tema8', function(){
+    return view('generic7');
+});
+
+Route::get('/tema9', function(){
+    return view('generic8');
+});
