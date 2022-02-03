00e8eea78164e2b39e2cec737c0e8fd6e8d860d9
Your Name <you@example.com>
2022-02-02 23:40:57 -0600

    PROYECTO

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('inicio');
+Route::get('/', function() {
+    return view('welcome');
 });
 
 Route::get('/tema1', function(){
